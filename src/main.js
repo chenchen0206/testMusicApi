c04987f752239b4dbd483dfd2a269f503f6bb68c
cx <1456735680@qq.com>
2024-07-05 17:44:56 +0800

    测试提交

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ const { SERVER_PORT } = require("./config/server");
 require("./utils/handle-error");
 // 2.将app启动起来
 app.listen(SERVER_PORT, () => {
-  console.log("coderhub的服务器启动成功~");
+  console.log("coderhub的服务器启动成功~测试提交");
 });
